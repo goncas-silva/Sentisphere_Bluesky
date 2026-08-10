52c9aec0719f624d2035a710788daeecf38547a6
goncas-silva <154056763+goncas-silva@users.noreply.github.com>
2026-08-10 18:43:05 +0100

    main.py : search functiionality, yet to be tested with login feature

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from atproto import Client
+
+client = Client()
+client.login(
+    "Username.bsky.social",
+    "PassWord"
+)
+
+#Subject intended to be searched
+
+Search = input("What do you wish to search?")
+
+Result = client.app.bsky.feed.search_posts(
+    params={
+        "q":Search,
+        "limit" : 10
+    }
+) 
+
+print(f"\n Posts that were found about: {Search} \n")
+
+for post in Result.posts:
+    print(post.record.text)
+    print()
+
+
+
 
